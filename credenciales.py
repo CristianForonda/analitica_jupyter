f397c77deb9eeda524c75e4f60f04be63b097794
Cristian <cristian.foronda@gmail.com>
2022-05-02 10:01:39 -0500

    inicial

---
+++ credenciales.py
@@ -0,0 +1,7 @@
+e_remitente = "micelular123258@gmail.com"  # cuenta de correo del que enviá
+e_contraseña = "4348644cristian"  # contraseña del correo del que enviá
+e_destinatario = [
+    "cristian.foronda123@gmail.com"
+]  # correo al que se envían los reportes
+e_asunto = "reportes PDF"  # asunto del correo
+e_mensaje = "Este es el mensaje del email."  # cuerpo del correo
